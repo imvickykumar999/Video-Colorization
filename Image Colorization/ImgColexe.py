bb7c10236cae03988e321d322ffebc9be1858334
Vicky Kumar <imvickykumar999@gmail.com>
2022-12-20 19:35:13 +0530

    Add files via upload

---
+++ Image Colorization/ImgColexe.py
@@ -0,0 +1,30 @@
+
+# >>> pip install pyinstaller
+# >>> pyinstaller --onefile --noconsole "ImgColexe.py"
+# >>> cd dist
+
+# put input.jpg in dist folder and double click on ImgColexe.exe
+
+import argparse
+import matplotlib.pyplot as plt
+from colorizers import *
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--use_gpu', action='store_true', help='whether to use GPU')
+opt = parser.parse_args()
+
+colorizer_eccv16 = eccv16(pretrained=True).eval()
+colorizer_siggraph17 = siggraph17(pretrained=True).eval()
+if(opt.use_gpu):
+	colorizer_eccv16.cuda()
+	colorizer_siggraph17.cuda()
+
+img = load_img('input.jpg')
+(tens_l_orig, tens_l_rs) = preprocess_img(img, HW=(256,256))
+if(opt.use_gpu):
+	tens_l_rs = tens_l_rs.cuda()
+
+img_bw = postprocess_tens(tens_l_orig, torch.cat((0*tens_l_orig,0*tens_l_orig),dim=1))
+out_img_eccv16 = postprocess_tens(tens_l_orig, colorizer_eccv16(tens_l_rs).cpu())
+out_img_siggraph17 = postprocess_tens(tens_l_orig, colorizer_siggraph17(tens_l_rs).cpu())
+plt.imsave('output.png', out_img_eccv16)
